18d60a4b80ff2997f636303ab60d5a02604705bd
Daniel Valladolid <danielvalladolidmoreno@gmail.com>
2025-10-05 09:56:20 +0200

    Cambios necesarios para render

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@@f58+v^zv%j$)hno#c*&m%#$if6z65vl4&1mm5-6xp2(s@ad9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
